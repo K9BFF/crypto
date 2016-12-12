3beac36cb88178d70e6b8a597c91b4dbb05bf7e1
Mason <masoncodes0@gmail.com>
2016-12-12 15:12:11 -0600

    Add files via upload

--- crypto.py
+++ crypto.py
@@ -1,5 +1,24 @@
 import base64
-import core
+
+
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = (ord(clear[i]) + ord(key_c)) % 256
+        enc.append(enc_c)
+    encryp = (base64.urlsafe_b64encode(bytes(enc)))
+    print(base64.urlsafe_b64encode(bytes(enc)))
+
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc)
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + enc[i] - ord(key_c)) % 256)
+        dec.append(dec_c)
+    print("".join(dec))
 
 
 encryp = ""
@@ -21,14 +40,14 @@ while go:
     if kind == 'encode':
         clear = input("String: ")
         key = input("Password: ")
-        core.encode(key, clear)
+        encode(key, clear)
         print("For decryption to work, only copy the text in quotes.\n")
 
     elif kind == 'decode':
         print("Only input the text inside the quotes.")
         enc = input("Encrypted Text: ")
         key = input("Password: ")
-        core.decode(key, enc)
+        decode(key, enc)
         print()
 
     elif kind == 'code':
